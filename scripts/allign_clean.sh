b5723ceececa7e7340b015dbe1a4cfa02297df4e
Bhargav Srinivasan <bds062@nexuscbcb00.umiacs.umd.edu>
2025-03-22 16:38:53 -0400

    updated allign_clean and added allign_trimmed

--- scripts/allign_clean.sh
+++ scripts/allign_clean.sh
@@ -4,7 +4,7 @@
 #Requires: Bowtie2 Installation
 #BDS 3/21/25
 
-#SBATCH --job-name=Bowtie2Test
+#SBATCH --job-name=Bowtie2Cleans
 #SBATCH -c 16
 #SBATCH --mem=128G
 #SBATCH --qos=high
@@ -13,24 +13,21 @@
 #SBATCH --time=540:00
 #SBATCH --mail-type=BEGIN,END,TIME_LIMIT
 #SBATCH --mail-user=bds062@terpmail.umd.edu
-#SBATCH -o ./Ja_testBowtie.txt
-#SBATCH -e ./Ja_testBowtie.txt
-
-../../programs/bowtie2/bowtie2 -t -x zea -1 clean_SRR18709356_1.fastq -2 clean_SRR18709356_2.fastq --very-sensitive -S test56.sam
-../../programs/bowtie2/bowtie2 -t -x zea -1 clean_SRR18709355_1.fastq -2 clean_SRR18709355_2.fastq --very-sensitive -S test55.sam
-../../programs/bowtie2/bowtie2 -t -x zea -1 clean_SRR18709348_1.fastq -2 clean_SRR18709348_2.fastq --very-sensitive -S test48.sam
+#SBATCH -o ./cleanAllignment.txt
+#SBATCH -e ./cleanAllignment.txt
 
 BOWTIE="../../programs/bowtie2/bowtie2"
 TAGS="-t -x zea"
 
-for file1 in ${FASTQ_DIR}/clean_*_1.fastq; do
+for file1 in ./clean_*_1.fastq; do
     # Extract the SRR ID
-    base_name=$(basename "$file" | sed -E 's/clean_(SRR[0-9]+)_1.fastq/\1/')
+    base_name=$(basename "$file1" | sed -E 's/clean_(SRR[0-9]+)_1.fastq/\1/')
     file2="clean_${base_name}_2.fastq"
 
-    if [[ -f "$read2_file" ]]; then
+    if [[ -f "$file2" ]]; then
         echo "Running Bowtie2 for $base_name..."
-        $BOWTIE $TAGS -1 $file1 -2 $file2 --very-sensitive -S $base_name.sam
+        $BOWTIE $TAGS -1 "$file1" -2 "$file2" --very-sensitive -S "${base_name}.sam"
         else
-        echo "Skipping $file2: Read 2 file not found."
+        echo "Skipping ${file1}: Read 2 file not found."
     fi
+done
